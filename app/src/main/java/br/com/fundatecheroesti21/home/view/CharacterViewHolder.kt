39c21e2e8cb2a8e9b574b3c33745c3fba9f0fa3a
Marcela P Proenca <marcelaproencaeng@gmail.com>
2023-08-19 17:52:01 -0300

    Ajuste CharacterViewHolder com glide

--- app/src/main/java/br/com/fundatecheroesti21/home/view/CharacterViewHolder.kt
+++ app/src/main/java/br/com/fundatecheroesti21/home/view/CharacterViewHolder.kt
@@ -3,13 +3,18 @@ package br.com.fundatecheroesti21.home.view
 import androidx.recyclerview.widget.RecyclerView
 import br.com.fundatecheroesti21.databinding.CharacterListItemBinding
 import br.com.fundatecheroesti21.home.presentation.model.CharacterModel
+import com.bumptech.glide.Glide
 
 class CharacterViewHolder(private val binding: CharacterListItemBinding) :
+
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(character: CharacterModel) {
         binding.tvName.text = character.name
         binding
+        Glide.with(binding.root.context)
+            .load(character.url)
+            .into(binding.image)
     }
 
 }
